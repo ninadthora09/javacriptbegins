ebfe30c41d885a58d52c04c81175650d7224320a
Ninad Thorat <150773623+ninadthora09@users.noreply.github.com>
2025-06-12 15:14:39 +0000

    done

--- basics/test.js
+++ basics/test.js
@@ -8,3 +8,13 @@ fun("thorat");
 
 console.log("hello hero");
 console.log("om thorat");
+
+ const val={
+    name:"ninad",
+    age:34,
+    gender:"male",
+ };
+
+ console.log(val.name);
+ console.log(val.gender);
+console.log("kay re kay karrto")
