4e8c3fb3bceeb5413a583b2626b94287d719cda1
Ninad Thorat <150773623+ninadthora09@users.noreply.github.com>
2025-06-14 16:22:12 +0000

    done

---
+++ basics/operatn.js
@@ -0,0 +1,48 @@
+//operation
+// let value = 4
+// let negvalue= -value
+// console.log(negvalue);
+
+// console.log(value**value);
+// console.log(value/value);
+// console.log(value%value);
+
+// let str= "hello"
+// let str2=" ninad";
+// console.log(str+str2);
+
+// console.log(1+2+"2"); //32
+// console.log("1"+2+2); // 122
+
+// console.log(+true); // not good or right way to use
+// console.log(+"")
+
+// gameCounter= 200;
+// ++gameCounter;
+// console.log(gameCounter);
+
+// let a=0
+// let y=a++
+// console.log(a) // 1
+// console.log(y) // 0
+
+
+// let k =9
+// let j=++k
+// console.log(k) // 9
+// console.log(j)// 9
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
