f7faebed4f1246edeff7ccbf924c95c1865422d0
Ninad Thorat <150773623+ninadthora09@users.noreply.github.com>
2025-06-14 16:39:45 +0000

    done

---
+++ basics/comp.js
@@ -0,0 +1,14 @@
+// console.log(2==1);
+// console.log(2!=1);
+
+// console.log("2">=1); // => true string converted to number
+// console.log(null>0); //=> false null converted to 0 
+// console.log(null>=0) // => true null is now 0 so 0==0
+
+
+// console.log(undefined==0); // => always false with any comparesion
+
+// console.log("2"===2); // compares the value as well as the datatypes
+// console.log("2"==2);
+
+
