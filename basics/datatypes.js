fd601c0f30ebb42ce3b49ba670d2091b4597c8c6
Ninad Thorat <150773623+ninadthora09@users.noreply.github.com>
2025-06-14 17:31:45 +0000

    done

--- basics/datatypes.js
+++ basics/datatypes.js
@@ -1,11 +1,11 @@
-let name="ninad";
-const age=9;
-bool = true;
-console.log(bool);
-let val; //undifined variable is declare and not decleared that is undefined
-console.log(val);
-val=null; // null mean totally empty not a 0;
-console.log(typeof val); // type is object
+// let name="ninad";
+// const age=9;
+// bool = true;
+// console.log(bool);
+// let val; //undifined variable is declare and not decleared that is undefined
+// console.log(val);
+// val=null; // null mean totally empty not a 0;
+// console.log(typeof val); // type is object
 
 // primitive and non-primitive(refrencetype)
 
@@ -15,7 +15,36 @@ console.log(typeof val); // type is object
 
 
 //Non-primitive(RefrenceType)(Refence of the memory is directly given to them)
-//
+// arrays, objects, functions
+
+// const heros=["ninad","bigboss","heroNumberOne"]; // array
+// let n= heros.length
+// for(let i=0;i<n;i++){ 
+//     console.log(heros[i]);  
+// }   
+
+// const person={
+//     name:"ninad",
+//     age:34
+// }
+// console.log(person.name);
+
+// const myFunction= function(val,value){
+//    return val*value
+// }
+// console.log(myFunction(4,10));
+
+
+// how to declare the Symbols
+// const id= Symbol('123') // uniqueness to the id
+// const bigNumber =4244254454255n //big number 
+
+// const bigInt =454243542n
+// console.log(typeof(bigInt));
+
+
+
+
 
 
 
