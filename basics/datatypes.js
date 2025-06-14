9d767f100f0f6bd9ccc42fab9a5f09f1135bf335
Ninad Thorat <150773623+ninadthora09@users.noreply.github.com>
2025-06-14 16:53:21 +0000

    done

--- basics/datatypes.js
+++ basics/datatypes.js
@@ -2,8 +2,20 @@ let name="ninad";
 const age=9;
 bool = true;
 console.log(bool);
-let val;
+let val; //undifined variable is declare and not decleared that is undefined
 console.log(val);
-val=null;
+val=null; // null mean totally empty not a 0;
 console.log(typeof val); // type is object
+
+// primitive and non-primitive(refrencetype)
+
+// => primitive 7 types(no memmory refrence is given copy of that variable is given)
+// string,Number,boolean,null,undefined,Symbol(gives the uniqueness),
+// BigInt(Just the big brother of Number)
+
+
+//Non-primitive(RefrenceType)(Refence of the memory is directly given to them)
+//
+
+
 
